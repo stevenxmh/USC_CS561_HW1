ce606eb7b35aeef1ab5ed9c666b46c4266121664
stevenxmh <Xmh911111@gmail.com>
2021-02-03 20:50:30 -0500

    A* completed

--- src/homework.java
+++ src/homework.java
@@ -23,7 +23,7 @@ public class homework {
 			Scanner scan = new Scanner(myFile);
 			while (scan.hasNextLine()) {
 				String data = scan.nextLine();
-				System.out.println(data);
+//				System.out.println(data);
 				inputs.add(data);
 			}
 			scan.close();
@@ -128,6 +128,13 @@ public class homework {
 			this.m = m;
 			this.cost = cost;
 			this.h = h;
+		}
+		
+		
+		
+		// return the estimated total cost
+		public int getEstimate() {
+			return this.cost + this.h;
 		}
 		
 		// print out in the format of c,r
@@ -274,7 +281,7 @@ public class homework {
 						// if unvisited and not blocked
 						if (visited[curr.r+d[0]][curr.c+d[1]] == 0 && canMove(map, maxHeight, curr.r, curr.c, curr.r+d[0], curr.c+d[1])){
 
-							// if the adjacent cell was queued
+							// if the adjacent cell was queued but not visited
 							if (memo[curr.r+d[0]][curr.c+d[1]] != null) {
 								// if the new path is cheaper
 								Cell next = memo[curr.r+d[0]][curr.c+d[1]];
@@ -330,7 +337,7 @@ public class homework {
 	 */
 	private static List<String> aStar(int row, int col, int r, int c, int maxHeight,
 			int[][] settlingSites, int[][] map) {
-		System.out.println("A*");
+//		System.out.println("A*");
 		List<String> result = new ArrayList<>();
 
 
@@ -342,7 +349,6 @@ public class homework {
 			
 			// mark start state as visited
 			int[][] visited = new int[row][col];
-			visited[r][c] = 1;
 			// keep track of parents
 			Map<Cell, Cell> parents = new HashMap<>();
 			// queue is order by f(n) = g(n) + h(n)
@@ -352,28 +358,72 @@ public class homework {
 			memo[r][c] = origin;
 
 			queue.add(origin);
-
-			Cell curr;
+			
+			Cell curr = queue.peek();
 			
 			while (!queue.isEmpty()) {
+				// dequeue, mark as visited
 				curr = queue.poll();
-
+				visited[curr.r][curr.c] = 1;
+				
+				// break if at goal state
+				if (curr.r == site[0] && curr.c == site[1]) break;
+				
+				for (int[] d : directions){
+					// check inbound
+					if (curr.r + d[0] >= 0 && curr.r + d[0] <= row-1 && curr.c + d[1] >= 0 && curr.c + d[1] <= col-1){
+						// calculate the weight
+						int weight = 10;
+						if (Math.abs(d[0])+Math.abs(d[1]) == 2) {
+							// weight is 14 if path is diagonal
+							weight = 14;
+						}
+						
+						// if unvisited and not blocked
+						if (visited[curr.r+d[0]][curr.c+d[1]] == 0 && canMove(map, maxHeight, curr.r, curr.c, curr.r+d[0], curr.c+d[1])){
+							// get g(n)
+							int cost = getCost(map, weight, curr.r, curr.c, curr.r+d[0], curr.c+d[1]);
+							// if the adjacent cell was queued but not visited
+							if (memo[curr.r+d[0]][curr.c+d[1]] != null) {
+								Cell next = memo[curr.r+d[0]][curr.c+d[1]];
+								
+								if (next.cost > curr.cost + cost ) {
+									// compare the estimate cost and update queue
+									next.cost = curr.cost + cost;
+									parents.put(next, curr);
+									// Rearrange the priority queue
+									queue.remove(next);
+									queue.add(next);
+								}
+								
+							}else {
+								// compute h(n) for the next Cell 
+								h = getHeuristic(curr.r+d[0], curr.c+d[1], r, c);
+								Cell next = new Cell(curr.r+d[0], curr.c+d[1], map[curr.r+d[0]][curr.c+d[1]], curr.cost + cost, h);
+								queue.add(next);
+								parents.put(next, curr);
+								memo[curr.r+d[0]][curr.c+d[1]] = next;
+							}
+						}
+						
+					}
+				}
 			}
 			
 			// Generate output
 			String output = "";
 
 			// if queue is empty and settlers didn't make it
-//			if (curr.r != site[0] || curr.c != site[1]){
-//				output = "FAIL";
-//			}else{
-//				// back track to origin
-//				while (curr != null) {
-//					output = " " + curr.toString() + output;
-//					curr = parents.get(curr);
-//				}
-//				output = output.substring(1);
-//			}
+			if (curr.r != site[0] || curr.c != site[1]){
+				output = "FAIL";
+			}else{
+				// back track to origin
+				while (curr != null) {
+					output = " " + curr.toString() + output;
+					curr = parents.get(curr);
+				}
+				output = output.substring(1);
+			}
 
 			result.add(output);
 		}
@@ -382,6 +432,16 @@ public class homework {
 
 	}
 
+	/*
+	 * Compute the straight line distance from (x,y) to (r,c)
+	 * Each vertical or horizontal unit distance is 10
+	 */
+	private static int getHeuristic(int x, int y, int r, int c) {
+		int vertical = 10 * Math.abs(r - x);
+		int horizontal = 10 * Math.abs(c - y) ; 
+		return (int) Math.sqrt((vertical * vertical) + (horizontal * horizontal));
+	}
+	
 	/*
 	 * Compare the height difference between 2 cells with maxHeight
 	 * return true if wagon can be moved from (x, y) to (r, c) which are row-column pairs
@@ -393,13 +453,16 @@ public class homework {
 	}
 	
 	/*
-	 * Compute the straight line distance from (x,y) to (r,c)
-	 * Each vertical or horizontal unit distance is 10
+	 * Computer the g(n) for A* from (x, y) to (r, c)
 	 */
-	private static int getHeuristic(int x, int y, int r, int c) {
-		int vertical = 10 * Math.abs(r - x);
-		int horizontal = 10 * Math.abs(c - y) ;
-		return (int) Math.sqrt((vertical * vertical) + (horizontal * horizontal));
+	private static int getCost(int[][] map, int weight, int x, int y, int r, int c) {
+		int cost = weight;
+		// calculate the height difference
+		int originHeight = map[x][y] < 0 ? Math.abs(map[x][y]) : 0;
+		int targetHeight = map[r][c] < 0 ? Math.abs(map[r][c]) : 0;
+		cost += Math.abs(originHeight - targetHeight);
+		int muddiness = targetHeight == 0 ? map[r][c] : 0;
+		return cost + muddiness;
 	}
 
 }
